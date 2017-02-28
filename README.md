9bc619be6c90f89b68e9e4bd567e540f7a0affa9
Stasiak207599 <207599@edu.p.lodz.pl>
2017-02-28 22:43:40 +0100

    Quickstart project commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProgramowanieJavaZaaw
+
+Testowy komentarz
---
+++ my-app/src/test/java/com/mycompany/app/JunitTest.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Michał on 2017-02-28.
+ */
+public class JunitTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(true, true);
+    }
+
+}
